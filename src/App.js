f117b60f084a1442da9e6177a93de43483fb3556
George McCadden III <thethirdone.g@gmail.com>
2021-06-09 08:44:07 -0700

    Fetching data from an API to set state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			zombies: [
-				{
-					name: 'Randy',
-					id: 'ran'
-				},
-				{
-					name: 'Simeon',
-					id: 'sim'
-				},
-				{
-					name: 'Rasputin',
-					id: 'ras'
-				}
-			]
+			zombies: []
 		};
 	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ zombies: users }))
+
+	}
+
 	render() {
 		return (
 			<div className="App">{this.state.zombies.map((zombie) => <h1 key={zombie.id}> {zombie.name} </h1>)}</div>
